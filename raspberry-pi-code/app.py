a7795b786726050ce3e6f49102223d51d41d62e4
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-20 21:43:00 +0000

    update

--- raspberry-pi-code/app.py
+++ raspberry-pi-code/app.py
@@ -1,31 +1,33 @@
-import datetime, time, math, serial, picamera
+import datetime, math, serial, picamera
 from geopy.distance import vincenty
 
 SERIAL_PORT_USED_BY_LINKITONE = '/dev/ttyACM0'
 FOLDER_TO_SAVE_IMAGES_TO = '/media/usbstick/'
 
+print 'About to take a picture...'
 camera = picamera.PiCamera()
 
-while True:
-    print 'About to take a picture...'   
+print 'Fetching latlong...'
+def get_latlon():
+    gps = serial.Serial(SERIAL_PORT_USED_BY_LINKITONE, 115200)
+    latlong = gps.readline()
+    lat = float(latlong.split(',')[0])
+    lon = float(latlong.split(',')[1][:-2]) # remove '\r\n' at the end
+    lon = lon/100 # accounts for bad Linkit One example code
+    return lat, lon
+lat, lon = get_latlon()
+print 'Found GPS location of {}, {}'.format(lat, lon)
 
-    # get latlong
-    def get_latlon():
-        gps = serial.Serial(SERIAL_PORT_USED_BY_LINKITONE, 115200)
-        latlong = gps.readline()
-        lat = float(latlong.split(',')[0])
-        lon = float(latlong.split(',')[1][:-2]) # remove '\r\n' at the end
-        lon = lon/100 # accounts for bad Linkit One example code
-        return lat, lon
-    lat, lon = get_latlon()
-    print 'Found GPS location of {}, {}'.format(lat, lon)
+print 'Checking if latlong is accurate...'
+latlong_is_accurate = True
+for x in range(2):
+    lat2, lon2 = get_latlon()
+    gps_readings_diff = vincenty((lat, lon), (lat2, lon2)).miles
+    if gps_readings_diff < 10:
+        latlong_is_accurate = False
+print 'latlong is accurate: ' + str(latlong_is_accurate)
 
-    # check latlong accuracy
-    for x in range(2):
-        lat2, lon2 = get_latlon()
-        gps_readings_diff = vincenty((lat, lon), (lat2, lon2)).miles
-        assert gps_readings_diff < 10, "GPS accuracy isn't good enough"
-
+if latlong_is_accurate:
     # set GPSLatitude (credit: http://bit.ly/1Rg78PV)
     camera.exif_tags['GPS.GPSLatitudeRef'] = 'N' if lat > 0 else 'S'
     alat = math.fabs(lat)
@@ -42,10 +44,8 @@ while True:
     slon = int(6000 * (60 * (alon - dlon) - mlon))
     camera.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/100' % (dlon, mlon, slon)
 
-    # take picture
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-    filename = timestamp + '.jpg'
-    camera.capture(FOLDER_TO_SAVE_IMAGES_TO + filename)
-    print 'Image saved as {}'.format(filename)
-
-    time.sleep(5)
+print 'Taking picture...'
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+filename = timestamp + '.jpg'
+camera.capture(FOLDER_TO_SAVE_IMAGES_TO + filename)
+print 'Image saved as {}'.format(filename)
