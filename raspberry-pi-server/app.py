b3c1978d593604f594423e704507e147da1cc65d
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-26 15:46:37 +0000

    update

--- raspberry-pi-server/app.py
+++ raspberry-pi-server/app.py
@@ -1,8 +1,8 @@
 import os, datetime
 from flask import Flask, render_template, send_from_directory, request
 
-#FOLDER_TO_SAVE_IMAGES_TO = '/media/usb'
-FOLDER_TO_SAVE_IMAGES_TO = '/Users/manoj/Downloads/usbstick'
+FOLDER_TO_SAVE_IMAGES_TO = '/media/usb'
+#FOLDER_TO_SAVE_IMAGES_TO = '/Users/manoj/Downloads/usbstick'
 
 app = Flask(__name__)
 
