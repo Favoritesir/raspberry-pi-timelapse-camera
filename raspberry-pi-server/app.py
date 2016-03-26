7764a84665145d88f1076156bd24760d42f09776
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-26 19:57:25 +0000

    update

--- raspberry-pi-server/app.py
+++ raspberry-pi-server/app.py
@@ -41,6 +41,4 @@ def delete_image():
     return "Deleting img: {}".format(image)
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port='5000', debug=True)
