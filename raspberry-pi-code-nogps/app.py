15caa167d7016cc375dacf3751de19dd8f954ff5
Manoj Nathwani <manoj_1993@hotmail.com>
2016-03-26 21:56:41 +0000

    update

--- raspberry-pi-code-nogps/app.py
+++ raspberry-pi-code-nogps/app.py
@@ -1,14 +1,5 @@
-import datetime, picamera
-
-FOLDER_TO_SAVE_IMAGES_TO = '/media/usb/nogps'
-
-print 'Initializing camera...'
-camera.hflip = True
+import picamera, datetime
 camera = picamera.PiCamera()
-camera.led = False
-
-print 'Taking picture...'
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H%M')
 filename = '{}/{}.{}'.format(FOLDER_TO_SAVE_IMAGES_TO, timestamp, 'jpg')
 camera.capture(filename)
-print '- Image saved as {}'.format(filename)
